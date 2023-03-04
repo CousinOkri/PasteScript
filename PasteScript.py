86e610d5e15cc94a8d59a347b2cfc477b4ce094f
CousinOkri <52662644+CousinOkri@users.noreply.github.com>
2023-03-04 13:30:33 +0530

    Added Script and Requirements file

---
+++ PasteScript.py
@@ -0,0 +1,13 @@
+from googlesearch import search
+import pyfiglet
+
+print(pyfiglet.figlet_format("-------------"))
+print(pyfiglet.figlet_format("PasteScript", font="bulbhead"))
+print(pyfiglet.figlet_format("-------------"))
+
+paste_sites = ["pastebin.com", "pastie.org",
+               "ghostbin.com", "justepaste.it", "paste4btc.com"]
+domain = input(" Enter a domain:\n")
+for psite in paste_sites:
+    for i in search('site:{} "{}"'.format(psite, domain)):
+        print(i)
